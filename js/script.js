61f51d62337080852467cf6bcdd7a2603efbeeb5
Cristian de los Santos Cedano <cristiancedano1030@gmail.com>
2018-10-23 20:03:00 -0400

    como hacer card con efecto hover 3d con html css jquery

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var card = $(".card");
+
+$(document).on("mousemove", function (e) {
+    
+    var ax = -($(window).innerWidth() / 2 - e.pageX) / 20;
+    var ay = ($(window).innerHeight() / 2 - e.pageY) / 20;
+    
+    card.attr("style", "transform: rotateY(" + ax + "deg) rotateX(" + ay + "deg)");
+});
